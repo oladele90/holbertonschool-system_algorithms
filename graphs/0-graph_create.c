ed65016ecaf0a6a784b68173ebc6291dce22ca59
oladele90 <oladelemiro07@gmail.com>
2023-11-17 20:17:18 +0000

    create

--- graphs/0-graph_create.c
+++ graphs/0-graph_create.c
@@ -3,11 +3,8 @@
 
 graph_t *graph_create(void)
 {
-    vertex_t *vertex = malloc(sizeof(vertex_t));
     graph_t *graph = malloc(sizeof(graph_t));
 
-    vertex = NULL;
-    graph->vertices = vertex;
     graph->nb_vertices = 0;
     return (graph);
 }
