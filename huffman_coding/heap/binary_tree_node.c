c03f7f8f527f1cc0c71840504ed070d366c267cd
oladele90 <oladelemiro07@gmail.com>
2023-11-28 01:12:45 +0000

    add node

--- huffman_coding/heap/binary_tree_node.c
+++ huffman_coding/heap/binary_tree_node.c
@@ -1,19 +1,26 @@
 #include "heap.h"
 
-
+/**
+ * binary_tree_node - Creates a new binary tree node.
+ * @parent: Pointer to the parent node.
+ * @data: Pointer to the data to be stored in the node.
+ *
+ * Return: Pointer to the newly created binary tree node, or NULL on failure.
+ */
 binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data)
 {
-    binary_tree_node_t *new;
+	binary_tree_node_t *new;
 
-    new = malloc(sizeof(binary_tree_node_t));
-    if (!new)
-        return NULL;
-    if (!data)
-        return NULL;
+	new = malloc(sizeof(binary_tree_node_t));
+	if (!new)
+		return (NULL);
+	if (!data)
+		return (NULL);
 
-    new->parent = parent;
-    new->left = NULL;
-    new->right = NULL;
-    new->data = data;
-    return (new);
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	new->data = data;
+
+	return (new);
 }
