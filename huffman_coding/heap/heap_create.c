549bef43788a4aa6b5e76d33eb0dd0d3b7e60973
oladele90 <oladelemiro07@gmail.com>
2023-11-28 00:02:08 +0000

    handles malloc return

--- huffman_coding/heap/heap_create.c
+++ huffman_coding/heap/heap_create.c
@@ -1,13 +1,22 @@
 #include "heap.h"
 
-
+/**
+ * heap_create - Creates a new heap.
+ * @data_cmp: Pointer to the comparison function for the heap data.
+ *
+ * Return: Pointer to the newly created heap, or NULL on failure.
+ */
 heap_t *heap_create(int (*data_cmp)(void *, void *))
 {
-    heap_t *new;
+	heap_t *new;
 
-    new = malloc(sizeof(heap_t));
-    new->data_cmp = data_cmp;
-    new->size = 0;
-    new->root = NULL;
-    return (new);
+	new = malloc(sizeof(heap_t));
+	if (!new)
+		return (NULL);
+
+	new->data_cmp = data_cmp;
+	new->size = 0;
+	new->root = NULL;
+
+	return (new);
 }
