84e5943887c536bbfb47dde441ba4aef6546176c
oladele90 <oladelemiro07@gmail.com>
2023-12-02 17:50:16 +0000

    priority que

---
+++ huffman_coding/huffman_priority_queue.c
@@ -0,0 +1,52 @@
+#include "huffman.h"
+#include "heap.h"
+
+/**
+ * data_cmp - Compares the frequencies of two symbols for heap ordering.
+ *
+ * @f1: Pointer to the first symbol.
+ * @f2: Pointer to the second symbol.
+ *
+ * Return: The difference in frequencies (f1 - f2).
+ */
+int data_cmp(void *f1, void *f2)
+{
+	int n1, n2;
+	symbol_t *one, *two;
+
+	one = (symbol_t *)f1;
+	two = (symbol_t *)f2;
+	n1 = (int)one->freq;
+	n2 = (int)two->freq;
+
+	return (n1 - n2);
+}
+
+/**
+ * huffman_priority_queue - Creates a priority queue for Huffman coding.
+ *
+ * @data: Array of characters representing symbols.
+ * @freq: Array of frequencies corresponding to each symbol.
+ * @size: Number of elements in the arrays.
+ *
+ * Return: Pointer to the created heap representing the priority queue.
+ */
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size)
+{
+	symbol_t *new_sym = NULL;
+	binary_tree_node_t *new_node = NULL;
+	heap_t *new_heap = NULL;
+	size_t idx = 0;
+
+	new_heap = heap_create(data_cmp);
+
+	while (idx < size)
+	{
+		new_sym = symbol_create(data[idx], freq[idx]);
+		new_node = binary_tree_node(new_heap->root, new_sym);
+		heap_insert(new_heap, new_node);
+		idx++;
+	}
+
+	return (new_heap);
+}
