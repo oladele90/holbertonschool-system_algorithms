c6b1e036d39db77a922781ebc618222036b3511e
oladele90 <oladelemiro07@gmail.com>
2023-12-02 16:45:29 +0000

    heap

--- huffman_coding/heap/heap_delete.c
+++ huffman_coding/heap/heap_delete.c
@@ -1,27 +1,27 @@
 #include "heap.h"
 /**
- * deleter - recursive function that frees data and nodes of a heap
- * @node: the current node to free
- * @free_data: function that frees the data in a heap node
+ * delete - recursive function that frees data and roots of a heap
+ * @root: the current root to free
+ * @free_data: function that frees the data in a heap root
 */
-static void deleter(binary_tree_node_t *node, void (*free_data)(void *))
+void delete(binary_tree_node_t *root, void (*free_data)(void *))
 {
-	if (!node)
+	if (!root)
 		return;
-	deleter(node->left, free_data);
-	deleter(node->right, free_data);
+	delete(root->left, free_data);
+	delete(root->right, free_data);
 	if (free_data)
-		free_data(node->data);
-	free(node);
+		free_data(root->data);
+	free(root);
 
 }
 /**
  * heap_delete - deletes a heap
  * @heap: pointer to a heap struct
- * @free_data: function pointer that frees a nodes stored data
+ * @free_data: function pointer that frees a roots stored data
 */
 void heap_delete(heap_t *heap, void (*free_data)(void *))
 {
-	deleter(heap->root, free_data);
+	delete(heap->root, free_data);
 	free(heap);
 }
