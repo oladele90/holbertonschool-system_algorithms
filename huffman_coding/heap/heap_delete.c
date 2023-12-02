990fce83984118035915e75f989491d4c5a7ce3c
oladele90 <oladelemiro07@gmail.com>
2023-12-02 12:54:46 +0000

    heap delete

---
+++ huffman_coding/heap/heap_delete.c
@@ -0,0 +1,55 @@
+#include "heap.h"
+/**
+ * get_last_node - Finds the last node in the heap using level order traversal.
+ * @heap: Pointer to the heap structure.
+ *
+ * Return: Pointer to the last node.
+ */
+
+binary_tree_node_t *get_last_nod(heap_t *heap)
+{
+	binary_tree_node_t *current;
+	binary_tree_node_t *Q[50];
+	binary_tree_node_t *last_node;
+	int idx = 0, count = 0;
+
+	if (heap->size == 0 || heap->root == NULL)
+		return (NULL);
+	Q[count++] = heap->root;
+
+	while (idx < count)
+	{
+		current = Q[idx++];
+
+		if (current->left != NULL)
+			Q[count++] = current->left;
+		if (current->right != NULL)
+			Q[count++] = current->right;
+	}
+
+	last_node = Q[count - 1];
+	return (last_node);
+}
+
+/**
+ * heap_delete - Deletes all nodes in the heap and frees associated memory.
+ * @heap: Pointer to the heap structure.
+ * @free_data: Pointer to the function used to free data in each node.
+ */
+
+void heap_delete(heap_t *heap, void (*free_data)(void *))
+{
+	binary_tree_node_t *node;
+
+	if (heap->size == 0 || !free_data)
+		return;
+
+	while (heap->size > 0)
+	{
+		node = get_last_nod(heap);
+		free_data(node);
+		free(node);
+		heap->size--;
+	}
+	free(heap);
+}
