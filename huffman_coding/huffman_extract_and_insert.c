dbd52e94ffb7e2fc4aa3abd0715158596056fa63
oladele90 <oladelemiro07@gmail.com>
2023-12-04 21:17:34 +0000

    in ex

---
+++ huffman_coding/huffman_extract_and_insert.c
@@ -0,0 +1,34 @@
+#include "huffman.h"
+#include "heap.h"
+
+#include "heap.h"
+
+/**
+ * huffman_extract_and_insert - Perform a Huffman coding step.
+ *
+ * @priority_queue: Pointer to the heap representing the priority queue.
+ *
+ * Return: 1 on success, 0 on failure.
+ */
+int huffman_extract_and_insert(heap_t *priority_queue)
+{
+	binary_tree_node_t *node1 = heap_extract(priority_queue);
+	binary_tree_node_t *node2 = heap_extract(priority_queue);
+	binary_tree_node_t *node;
+	symbol_t *symbol;
+
+	if (!priority_queue || priority_queue->size < 2)
+		return (0);
+
+	symbol = symbol_create(-1, ((symbol_t *)node1->data)->freq +
+							((symbol_t *)node2->data)->freq);
+
+	node = binary_tree_node(NULL, symbol);
+
+	node->left = node1;
+	node->right = node2;
+
+	heap_insert(priority_queue, node);
+
+	return (1);
+}
