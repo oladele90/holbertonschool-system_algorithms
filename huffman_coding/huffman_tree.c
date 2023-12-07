6baefddda25772013400e214e7bcf8cb38c154ca
oladele90 <oladelemiro07@gmail.com>
2023-12-07 17:01:32 +0000

    fixes betty style

--- huffman_coding/huffman_tree.c
+++ huffman_coding/huffman_tree.c
@@ -12,17 +12,17 @@
  */
 binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
 {
-    heap_t *priority_queue = NULL;
-    binary_tree_node_t *root = NULL;
+	heap_t *priority_queue = NULL;
+	binary_tree_node_t *root = NULL;
 
-    priority_queue = huffman_priority_queue(data, freq, size);
+	priority_queue = huffman_priority_queue(data, freq, size);
 
-    if (!priority_queue)
-        return NULL;
+	if (!priority_queue)
+		return (NULL);
 
-    while (priority_queue->size > 1)
-        huffman_extract_and_insert(priority_queue);
-    root = heap_extract(priority_queue);
-    heap_delete(priority_queue, NULL);
-    return (root);
+	while (priority_queue->size > 1)
+		huffman_extract_and_insert(priority_queue);
+	root = heap_extract(priority_queue);
+	heap_delete(priority_queue, NULL);
+	return (root);
 }
