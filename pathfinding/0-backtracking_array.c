a17e11fdb1506ba76e7ae6b0e1b72799ac86785c
oladele90 <oladelemiro07@gmail.com>
2023-12-12 19:35:41 +0000

    even sadder

--- pathfinding/0-backtracking_array.c
+++ pathfinding/0-backtracking_array.c
@@ -33,59 +33,67 @@ point_t *pointdup(point_t *start)
     return (new_point);
 }
 
-queue_t *find_path(queue_t *new, char **map, int rows, int cols, point_t *start, point_t const *target)
+int find_path(queue_t *new, char **map, int rows, int cols, point_t *start, point_t const *target, char **visited)
 {
     int on_target_path = 0;
 
     if (start->x >= rows || start->y >= cols)
-        return (new);
+        return (0);
 
     printf("Checking coordinates [%d, %d]\n", start->x, start->y);
 
     if (start->x == target->x && start->y == target->y)
     {
-        printf("herehere\n");
-        queue_push_front(new, start);
-        return (new);
+        queue_push_front(new, (void *)pointdup(start));
+        return (1);
     }
-
-    if (map[start->y][start->x] == '0')
+    visited[start->y][start->x] = '1';
+    on_target_path += find_path(new, map, rows, cols, right(start), target, visited);
+    left(start);
+    if (on_target_path)
     {
-        
-        if (find_path(new, map, rows, cols, right(start), target))
-            on_target_path = 1;
-        left(start);
-        if (find_path(new, map, rows, cols, down(start), target))
-            on_target_path = 1;
-        up(start);
-        if (find_path(new, map, rows, cols, left(start), target))
-            on_target_path = 1;
-        right(start);
-        if (find_path(new, map, rows, cols, up(start), target))
-            on_target_path = 1;
-        down(start);
-
-        if (on_target_path)
-        {
-            map[start->y][start->x] = '1';
-            queue_push_front(new, pointdup(start));
-            return (new);
-        }
-
-        dequeue(new);
-
+        queue_push_front(new, (void *)pointdup(start));
+        return (1);
     }
-    return (NULL);
+    on_target_path += find_path(new, map, rows, cols, down(start), target, visited);
+    up(start);
+    if (on_target_path)
+    {
+        queue_push_front(new, (void *)pointdup(start));
+        return (1);
+    }
+    on_target_path += find_path(new, map, rows, cols, left(start), target, visited);
+    right(start);
+    if (on_target_path)
+    {
+        queue_push_front(new, (void *)pointdup(start));
+        return (1);
+    }
+    on_target_path += find_path(new, map, rows, cols, up(start), target, visited);
+    down(start);
+
+    if (on_target_path)
+    {
+        queue_push_front(new, (void *)pointdup(start));
+        return (1);
+    }
+
+    return (0);
 }
 queue_t *backtracking_array(char **map, int rows, int cols, point_t const *start, point_t const *target)
 {
+    int i;
     point_t *temp = malloc(sizeof(point_t));
     queue_t *new = queue_create();
+    char **visited = (char **)calloc(rows, sizeof(char *));
+
+    for (i = 0; i < rows; ++i)
+        visited[i] = (char *)calloc(cols, sizeof(char));
 
     temp->x = start->x;
     temp->y = start->y;
 
-    new = find_path(new, map, rows, cols, temp, target);
+    find_path(new, map, rows, cols, temp, target, visited);
 
     return (new);
 }
