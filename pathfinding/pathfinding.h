bd6123ec66fd888c770d3f8534deadc5a98d1f44
oladele90 <oladelemiro07@gmail.com>
2023-12-13 14:49:48 +0000

    adds betty

--- pathfinding/pathfinding.h
+++ pathfinding/pathfinding.h
@@ -14,11 +14,12 @@
  */
 typedef struct point_s
 {
-    int x;
-    int y;
+	int x;
+	int y;
 } point_t;
 
 
-queue_t *backtracking_array(char **map, int rows, int cols, point_t const *start, point_t const *target);
+queue_t *backtracking_array(char **map, int rows, int cols,
+							point_t const *start, point_t const *target);
 
 #endif
